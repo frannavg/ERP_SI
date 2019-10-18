1ba0e2720a6491a23245ef10ac41bbb32eba2aff
frannavg <fran.nav.g@gmail.com>
2019-10-18 08:34:28 -0300

    Subindo src

---
+++ ERP_source/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\francisco\desktop\erp-si\erp_source\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
