8a79c9795e29c8814deb902d64dcfb2c22d15a18
breno <breno.peixoto.santos@gmail.com>
2019-10-22 12:33:53 -0300

    adicionado data da ultima compra

--- ERP_source/ERP/produto/forms.py
+++ ERP_source/ERP/produto/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Produto
 
 class PostProduto(forms.ModelForm):
-
+    dataCompra = forms.DateField(input_formats=['%d/%m/%Y'])
     class Meta:
         model = Produto
-        fields = ('fkCategoria', 'precoCompra','name','precoVenda','qtde','fornecedor',)
+        fields = ('fkCategoria', 'precoCompra','name','precoVenda','qtde','fornecedor','dataCompra',)
